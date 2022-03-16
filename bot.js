2c4819c9843286188277fd4cce43b4d65f0bd207
Joshua Oladeji <dejijolaoluwa@gmail.com>
2022-03-16 16:32:35 +0100

    added logic to get latest blocks every 3 seconds

--- bot.js
+++ bot.js
@@ -8,27 +8,23 @@ const traderjoe = require('./contractABIs/traderjoe');
 const { abi } = require('@pangolindex/exchange-contracts/artifacts/contracts/pangolin-core/interfaces/IPangolinPair.sol/IPangolinPair.json');
 
 // addresses of the coins present in the pair we want to check
-const baseCoinAddress = '0x63a72806098Bd3D9520cC43356dD78afe5D386D9'; // AAVE - mainnet
-const quoteCoinAddress = '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7'; // WAVAX - mainnet
+const otherCoinAddress = '0x63a72806098Bd3D9520cC43356dD78afe5D386D9'; // AAVE - mainnet
+const nativeCoinAddress = '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7'; // WAVAX - mainnet (The native currency of the avalanche C-chain)
 
-// web3 instances for the fuji testnet and avalanche mainnet
-const testnetWeb3 = new Web3(process.env.AVAX_FUJI_NODE_URL);
-const mainnetWeb3 = new Web3(process.env.AVAX_MAINNET_NODE_URL);
+// web3 instances for the fuji testnet and avalanche mainnet on HTTPS and web socket
+const web3HttpsTestnet = new Web3(process.env.MORALIS_FUJI_NODE);
+const web3WebSocketTestnet = new Web3(process.env.MORALIS_FUJI_WEB_SOCKET_NODE);
+const web3HttpsMainnet = new Web3(process.env.MORALIS_MAINNET_NODE);
+const web3WebSocketMainnet = new Web3(process.env.MORALIS_MAINNET_WEB_SOCKET_NODE);
 
 
-// const getPairAddress = async (_factoryContract, _baseCoinAddress, _quoteCoinAddress) => {
-//   const rate = await _factoryContract.methods.getPair(_baseCoinAddress, _quoteCoinAddress).call();
-//   return rate;
-// }
-
-const getPairRatio = async (_exchangeAbi, _web3Instance, _factoryContract, _baseCoinAddress, _quoteCoinAddress) => {
+const getPangolinPairReserves = async (_web3Instance, _factoryContract, _otherCoinAddress) => {
   try {
-    const pairAddress = await _factoryContract.methods.getPair(_baseCoinAddress, _quoteCoinAddress).call();
-    const ExchangeContract = await new _web3Instance.eth.Contract(_exchangeAbi, pairAddress);
+    const pairAddress = await _factoryContract.methods.getPair(_otherCoinAddress, nativeCoinAddress).call();
+    const ExchangeContract = await new _web3Instance.eth.Contract(abi, pairAddress);
     const reserves = await ExchangeContract.methods.getReserves().call();
-    const { reserve0, reserve1 } = await reserves;
-    const ratio = Number(reserve1) / Number(reserve0);
-    return ratio;
+    console.log(reserves);
+    return reserves;
   }
   catch (error) {
     console.log(new Error(error.message));
@@ -36,28 +32,53 @@ const getPairRatio = async (_exchangeAbi, _web3Instance, _factoryContract, _base
   }
 }
 
+// const getTraderjoePairReserves = async (_web3Instance, _factoryContract, _otherCoinAddress) => {
+//   try {
+//     const pairAddress = await _factoryContract.methods.getPair(_otherCoinAddress, nativeCoinAddress).call();
+//     const ExchangeContract = await new _web3Instance.eth.Contract(traderjoe['EXCHANGE_ABI'], pairAddress);
+//     // const reserves = await ExchangeContract.methods.getReserves().call();
+//     console.log(typeof ExchangeContract.methods);
+//   }
+//   catch (error) {
+//     console.log(new Error(error.message));
+//     return 0;
+//   }
+// }
 
-const ArbitrageContract = new testnetWeb3.eth.Contract(arbitrageABI, arbitrageAddress);
-// we now have the contract to call the flash loan 👆
+
+const ArbitrageContract = new web3HttpsTestnet.eth.Contract(arbitrageABI, arbitrageAddress);
+// we now have the contract to call the flash loan 👆 - Note: it's on fuji testnet
 
 
 // TODO
 // 1. listen to mined blocks on the avalanche blockchain
+const checkLatestBlock = async () => {
+  try {
+    const latestBlock = await web3HttpsMainnet.eth.getBlock('latest');
+    const latestBlockNumber = latestBlock['number'];
+    console.log(latestBlock);
+
+    setTimeout(() => checkLatestBlock(), 3 * 1000);
+  }
+  catch (err) {
+    console.log(new Error(err.message));
+  }
+};
+// checkLatestBlock();
+
 
 // 2. get given crypto pair rate from pangolin
 const pangolinFactoryAddress = pangolin['ADDRESS'];
 const pangolinFactoryABI = pangolin['ABI'];
-const PangolinFactoryContract = new mainnetWeb3.eth.Contract(pangolinFactoryABI, pangolinFactoryAddress);
-getPairRatio(abi, mainnetWeb3, PangolinFactoryContract, baseCoinAddress, quoteCoinAddress);
+const PangolinFactoryContract = new web3HttpsMainnet.eth.Contract(pangolinFactoryABI, pangolinFactoryAddress);
+// getPangolinPairReserves(mainnetWeb3, PangolinFactoryContract, otherCoinAddress);
 
 
 // 3. get given crypto pair rate from trader joe
 // const traderjoeFactoryAddress = traderjoe['ADDRESS'];
 // const traderjoeFactoryABI = traderjoe['ABI'];
-// const TraderjoeFactoryContract = new mainnetWeb3.eth.Contract(traderjoeFactoryABI, traderjoeFactoryAddress);
-
-// getPairAddress(PangolinFactoryContract, baseCoinAddress, quoteCoinAddress);
-
+// const TraderjoeFactoryContract = new web3HttpsMainnet.eth.Contract(traderjoeFactoryABI, traderjoeFactoryAddress);
+// getTraderjoePairReserves(mainnetWeb3, TraderjoeFactoryContract, otherCoinAddress);
 
 
 // 4. find significant difference between prices
