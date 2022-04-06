2ca63cc978eaff94e2c97c5c9f01711f2e0875b6
Joshua Oladeji <dejijolaoluwa@gmail.com>
2022-04-06 03:39:06 +0100

    changed callback to async await in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,20 +4,23 @@ require('dotenv/config.js');
 const main = async () => {
   const pangolinFactory = '0xefa94DE7a4656D787667C749f7E1223D71E9FD88';
   const traderjoeRouter = '0x60aE616a2155Ee3d9A68541Ba4544862310933d4';
-  const [deployer] = await ethers.getSigners();
 
-  console.log("Deploying contracts with the account:", deployer.address);
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  const FlashSwapper = await ethers.getContractFactory("FlashSwapper");
-  const FlashSwapperContract = await FlashSwapper.deploy(pangolinFactory, traderjoeRouter);
-
-  console.log("FlashSwapper address:", FlashSwapperContract.address);
+  try {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const FlashSwapper = await ethers.getContractFactory("FlashSwapper");
+    const FlashSwapperContract = await FlashSwapper.deploy(pangolinFactory, traderjoeRouter);
+  
+    console.log("FlashSwapper address:", FlashSwapperContract.address);
+    process.exit(0)
+  }
+  catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
